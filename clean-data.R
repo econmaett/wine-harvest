734c9764311b13c1c721dfa1d6c8b2f64c97185c
EconMaett <matthias.spichiger@bluewin.ch>
2025-10-26 18:56:19 +0100

    changed span in geom_smooth to 0.1

--- clean-data.R
+++ clean-data.R
@@ -1,3 +1,4 @@
+# clan-data.R ----
 ## load packages ----
 library(tidyverse)
 
@@ -40,16 +41,16 @@ write_csv(x = wineharvest_long, file = "wineharvest_long.csv")
 ## plot ----
 wineharvest_caption <- "Daux, V., Garcia de Cortazar-Atauri, I., Yiou, P., Chuine, I., Garnier, E., Le Roy Ladurie, E., Mestre, O., and Tardaguila, J.: An open-access database of grape harvest dates for climate research: data description and quality assessment, Clim. Past, 8, 1403–1418, https://doi.org/10.5194/cp-8-1403-2012, 2012."
 
-p <- ggplot(data = wineharvest_wide, mapping = aes(x = year, y = bur)) +
+p <- ggplot(data = wineharvest_wide, mapping = aes(x = year, y = swi)) +
   geom_hline(yintercept = 0, linewidth = 0.5, colour = "darkgray") +
   geom_line(linewidth = 0.5, colour = "gray", alpha = 0.25) +
   geom_point(size = 1) +
-  geom_smooth(method = "loess", formula = "y ~ x", se = TRUE, colour = "#800020", fill = "#800020", span = 0.3, alpha = 0.2) +
-  # scale_x_continuous(breaks = seq(1500, 2050, 50), limits = c(1500, NA)) +
-  scale_x_continuous(breaks = seq(1350, 2050, 50)) +
+  geom_smooth(method = "loess", formula = "y ~ x", se = TRUE, colour = "#800020", fill = "#800020", span = 0.1, alpha = 0.2) +
+  scale_x_continuous(breaks = seq(1450, 2050, 50), limits = c(1450, NA)) +
+  # scale_x_continuous(breaks = seq(1350, 2050, 50)) +
   scale_y_continuous(breaks = seq(-10, 70, 20)) +
   labs(
-    title = "Annual wine harvest in Burgundy, France",
+    title = "Annual wine harvest in Switzerland (Leman Lake",
     subtitle = "Number of days after August 31",
     x = NULL, y = NULL,
     caption = str_wrap(wineharvest_caption)
@@ -59,7 +60,7 @@ p <- ggplot(data = wineharvest_wide, mapping = aes(x = year, y = bur)) +
 
 print(p)
 
-ggsave(filename = "burgundy.png", plot = p, path = ".", width = 24, height = 12, units = "cm", bg = "white")
+ggsave(filename = "switzerland.png", plot = p, path = ".", width = 24, height = 12, units = "cm", bg = "white")
 
 variable_selection <- c("bur", "jur", "srv", "swi")
 
@@ -67,7 +68,7 @@ p <- ggplot(data = wineharvest_long |> filter(abbreviation %in% variable_selecti
   # geom_hline(yintercept = 0, linewidth = 0.5, colour = "darkgray") +
   geom_line(linewidth = 0.5, colour = "gray", alpha = 0.25) +
   geom_point(size = 1) +
-  geom_smooth(method = "loess", formula = "y ~ x", se = TRUE, colour = "#800020", fill = "#800020", span = 0.3, alpha = 0.2) +
+  geom_smooth(method = "loess", formula = "y ~ x", se = TRUE, colour = "#800020", fill = "#800020", span = 0.1, alpha = 0.2) +
   facet_wrap(facets = vars(name), ncol = 1, scale = "free_y") +
   scale_x_continuous(breaks = seq(1350, 2050, 50)) +
   # scale_y_continuous(breaks = seq(-10, 70, 20)) +
